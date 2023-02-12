a0bd26ea3fe0eaf650feff45036138dbe5c5dde1
navrat <31410799+navrat@users.noreply.github.com>
2023-02-13 00:47:11 +0530

    Update utils.py

--- S7_CIFAR_RESNET_GRADCAM/utils.py
+++ S7_CIFAR_RESNET_GRADCAM/utils.py
@@ -29,20 +29,6 @@ import torch.nn.init as init
     - model_eval_f: to plot overall and class wise performance measures for the model on selected dataset
 '''
 
-def get_mean_and_std(dataset):
-    '''Compute the mean and std value of dataset.'''
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2)
-    mean = torch.zeros(3)
-    std = torch.zeros(3)
-    print('==> Computing mean and std..')
-    for inputs, targets in dataloader:
-        for i in range(3):
-            mean[i] += inputs[:,i,:,:].mean()
-            std[i] += inputs[:,i,:,:].std()
-    mean.div_(len(dataset))
-    std.div_(len(dataset))
-    return mean, std
-
 def init_params(net):
     '''Init layer parameters.'''
     for m in net.modules():
