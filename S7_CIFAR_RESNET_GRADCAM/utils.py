2a8d62681f414504367d71e27002c760eff41239
navrat <31410799+navrat@users.noreply.github.com>
2023-02-12 22:58:14 +0530

    creating the base utils file

--- S7_CIFAR_RESNET_GRADCAM/utils.py
+++ S7_CIFAR_RESNET_GRADCAM/utils.py
@@ -1,34 +1,115 @@
 # import necessary libraries
-import matplotlib.pyplot as plt
 import torch
-from data_loader import unnormalize
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import torchvision
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
-# define a function to plot misclassified images
-def plot_misclassified_images(model, test_loader, classes, device):
-    # set model to evaluation mode
-    model.eval()
-
-    misclassified_images = []
-    actual_labels = []
-    predicted_labels = []
-    
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            _, pred = torch.max(output, 1)
-            for i in range(len(pred)):
-                if pred[i] != target[i]:
-                    misclassified_images.append(data[i])
-                    actual_labels.append(classes[target[i]])
-                    predicted_labels.append(classes[pred[i]])
-
-    # Plot the misclassified images
-    fig = plt.figure(figsize=(12, 5))
-    for i in range(10):
-        sub = fig.add_subplot(2, 5, i+1)
-        npimg = unnormalize(misclassified_images[i].cpu())
-        plt.imshow(npimg, cmap='gray', interpolation='none')
-        sub.set_title("Actual: {}, Pred: {}".format(actual_labels[i], predicted_labels[i]),color='red')
-    plt.tight_layout()
+def unnormalize(tensor, mean=(0.4914, 0.4822, 0.4471), std=(0.2469, 0.2433, 0.2615)):
+    for t, m, s in zip(tensor, mean, std):
+        t.mul_(s).add_(m)
+    return tensor
+  
+def imshow(img):
+    img = unnormalize(img)    # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
+
+def plot_sample_images(dataloader_obj, target_classes, batch_size=5):
+    # get some random training images
+    dataiter = iter(dataloader_obj)
+    images, labels = next(dataiter)
+    # show images
+    imshow(torchvision.utils.make_grid(images[0:batch_size]))
+    # print labels
+    print([target_classes[i] for i in labels[0:batch_size]])
+
+def data_summary_stats(dataset):
+    # Load train data as numpy array
+    train_data =dataset.data
+    test_data = dataset.data
+
+    total_data = np.concatenate((train_data, test_data), axis=0)
+    print(total_data.shape)
+    print(total_data.mean(axis=(0,1,2))/255)
+    print(total_data.std(axis=(0,1,2))/255)
+        
+def plot_mispredictions(model, device, test_loader, target_classes):
+  '''
+  - provides accuracy and loss for the dataset created via test_loader
+  - provides a crosstab of all mispredictions to identify where model commonly fails
+  - takes the first 10 mispredictions and plots them on a 5X2 grid
+  '''
+  model.eval()
+
+  train_losses = []
+  test_losses = []
+  train_acc = []
+  test_acc = []
+  test_loss = 0
+  correct = 0
+  preds = []
+  targets = []
+  incorrect_pred_targets = []
+  incorrect_preds = []
+  incorrect_pred_images = []
+  incorrect_pred_images_all = []
+
+  with torch.no_grad():
+      for data, target in test_loader:
+          data, target = data.to(device), target.to(device)
+          # print(data.shape)
+          output = model(data)
+          test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+          pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+          correct += pred.eq(target.view_as(pred)).sum().item()
+          incorrects = ~pred.eq(target.view_as(pred))
+          if len(target[incorrects.squeeze(dim=1)])>0:
+            incorrect_pred_targets.append(target[incorrects.squeeze(dim=1)])
+            incorrect_preds.append(pred[incorrects.squeeze(dim=1)])
+            incorrect_pred_images.append(data[incorrects.squeeze(dim=1)])
+          [element for sublist in a for element in sublist]
+  incorrect_pred_targets_all = [t.cpu().numpy() for t in incorrect_pred_targets]
+  incorrect_pred_targets_all = [element for sublist in incorrect_pred_targets_all for element in sublist]
+  incorrect_preds_all = [t.cpu().numpy() for t in incorrect_preds]
+  incorrect_preds_all = [element for sublist in incorrect_preds_all for element in sublist]
+  incorrect_preds_all = [element for sublist in incorrect_preds_all for element in sublist]
+  incorrect_pred_images_all = [element for sublist in incorrect_pred_images for element in sublist]
+
+  print("\n # of incorrect images predicted in test dataset of {}: {}".format(len(test_loader.dataset), len(incorrect_pred_images_all)))
+  figure = plt.figure(figsize = (10,12))
+  num_of_images = 10
+  for index in range(1, num_of_images + 1):
+      plt.subplot(5, 2, index)
+      # plt.axis('off')
+#       print(incorrect_pred_images_all[index].cpu().numpy().shape)
+      plt.imshow(unnormalize(incorrect_pred_images_all[index].cpu()))
+      plt.title(f'Target- {target_classes[incorrect_pred_targets_all[index]]} ; Predicted- {target_classes[incorrect_preds_all[index]]}')
+
+  print("\n\n crosstab of incorrect prediction to understand patterns: \n")
+  return pd.crosstab(pd.Series([target_classes[i] for i in incorrect_pred_targets_all], name='Actual'), pd.Series([target_classes[i] for i in incorrect_preds_all], name='Predicted'))
+
+def model_viz(metrics_m1, train_val = ["training","validation"], metric = ["loss","accuracy"]):
+  """
+  Visualization of 3 models and their 4 sets of metrics (train/validation X loss/accuracy)
+  Each metrics_mi is a list expected in the order of [train_loss,validation_loss, train_accuracy, validation_accuracy]
+  """
+  import numpy as np
+  fig, axs = plt.subplots(2,2, figsize = (18,10))
+  for i in np.arange(0,2): # represents loss/accuracy
+    for j in np.arange(0,2): # represents train/validation
+      axs[i,j].plot(metrics_m1[i+j])
+#       axs[i,j].plot(metrics_m2[i+j])
+#       axs[i,j].plot(metrics_m3[i+j])
+      axs[i,j].set_xlabel('# epochs')
+      axs[i,j].set_ylabel(f"{train_val[j]} {metric[i]}")
+      axs[i,j].legend(['Model C1C2C3C4O'], loc = 'upper right')  
+      axs[i,j].set_title(f"{train_val[j]} {metric[i]} across the model")
+  fig.savefig('./experiments.png')
