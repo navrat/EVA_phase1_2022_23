992daaabfae41f32ee5e8d9e98b1fb9789e20279
navrat <31410799+navrat@users.noreply.github.com>
2023-02-12 14:03:28 +0530

    Update evaluate.py
    updated to include classwise classification report

--- S6_Dilated_Convolution_Albumentation/evaluate.py
+++ S6_Dilated_Convolution_Albumentation/evaluate.py
@@ -1,35 +1,53 @@
+# single evaluation function to provide the entire classification model performance against given dataset 
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import torchvision
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
-def calAccuracy(net, dataloader, device):
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for data in dataloader:
-            images, labels = data
-            images,labels = images.to(device),labels.to(device)
-            outputs = net(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+def model_eval_f(model, device, test_loader, target_classes):
+  '''
+  - provides accuracy and loss for the dataset created via test_loader
+  - provides a classwise accuracy table and classwise confusion matrix
+  '''
+  model.eval()
 
-    print(f'Accuracy of the network on the  train images: {(100 * correct / total)} %%')
+  train_losses = []
+  test_losses = []
+  train_acc = []
+  test_acc = []
+  test_loss = 0
+  preds = []
+  targets = []
 
-def calClassAccuracy(net, dataloader, classes, device):
-    class_correct = list(0. for i in range(10))
-    class_total = list(0. for i in range(10))
-    with torch.no_grad():
-        for data in dataloader:
-            images, labels = data
-            images,labels = images.to(device),labels.to(device)
-            outputs = net(images)
-            _, predicted = torch.max(outputs, 1)
-            c = (predicted == labels).squeeze()
-            for i in range(4):
-                label = labels[i]
-                class_correct[label] += c[i].item()
-                class_total[label] += 1
+  with torch.no_grad():
+      for data, target in test_loader:
+          data, target = data.to(device), target.to(device)
+          # print(data.shape)
+          output = model(data)
+          test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+          pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+          correct += pred.eq(target.view_as(pred)).sum().item()
+          incorrects = ~pred.eq(target.view_as(pred))
+          preds.append(pred)
+          targets.append(target)
+  preds = [elem.squeeze(dim=1).cpu().numpy() for elem in preds]
+  preds  = [element for sublist in preds for element in sublist]
+  targets = [element for sublist in targets for element in sublist]
+  test_loss /= len(test_loader.dataset)
+  test_losses.append(test_loss)
 
-
-    for i in range(10):
-        print('Accuracy of %5s : %2d %%' % (
-            classes[i], 100 * class_correct[i] / class_total[i]))
+  print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+      test_loss, correct, len(test_loader.dataset),
+      100. * correct / len(test_loader.dataset)))
+    
+  print('\n class wise performance: \n')
+  conf_matrix_all = confusion_matrix([target_classes[i] for i in targets], [target_classes[i] for i in preds])
+  print(pd.DataFrame({'classes':target_classes,'accuracy': 100*conf_matrix_all.diagonal()/conf_matrix_all.sum(axis=1)}))
+  print(classification_report([target_classes[i] for i in targets], [target_classes[i] for i in preds]))
