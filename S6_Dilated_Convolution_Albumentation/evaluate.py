829ac6d97e02f18e7a9272ffab2f83af3822c959
navrat <31410799+navrat@users.noreply.github.com>
2023-02-12 16:48:58 +0530

    Update evaluate.py

--- S6_Dilated_Convolution_Albumentation/evaluate.py
+++ S6_Dilated_Convolution_Albumentation/evaluate.py
@@ -25,6 +25,8 @@ def model_eval_f(model, device, test_loader, target_classes):
   test_loss = 0
   preds = []
   targets = []
+  correct = 0
+  incoorects=0
 
   with torch.no_grad():
       for data, target in test_loader:
@@ -51,3 +53,64 @@ def model_eval_f(model, device, test_loader, target_classes):
   conf_matrix_all = confusion_matrix([target_classes[i] for i in targets], [target_classes[i] for i in preds])
   print(pd.DataFrame({'classes':target_classes,'accuracy': 100*conf_matrix_all.diagonal()/conf_matrix_all.sum(axis=1)}))
   print(classification_report([target_classes[i] for i in targets], [target_classes[i] for i in preds]))
+
+
+def unnormalize(tensor, mean=(0.4914, 0.4822, 0.4471), std=(0.2469, 0.2433, 0.2615)):
+    for t, m, s in zip(tensor, mean, std):
+        t.mul_(s).add_(m)
+    return tensor
+  
+
+def plot_mispredictions(model, device, test_loader, target_classes, fig_size=(10,12), num_images=10):
+  '''
+  - provides accuracy and loss for the dataset created via test_loader
+  - provides a crosstab of all mispredictions to identify where model commonly fails
+  - takes the first 10 mispredictions and plots them on a 5X2 grid
+  '''
+  model.eval()
+
+  train_losses = []
+  test_losses = []
+  train_acc = []
+  test_acc = []
+  test_loss = 0
+  correct = 0
+  preds = []
+  targets = []
+  incorrect_pred_targets = []
+  incorrect_preds = []
+  incorrect_pred_images = []
+  incorrect_pred_images_all = []
+
+  with torch.no_grad():
+      for data, target in test_loader:
+          data, target = data.to(device), target.to(device)
+          # print(data.shape)
+          output = model(data)
+          test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+          pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+          correct += pred.eq(target.view_as(pred)).sum().item()
+          incorrects = ~pred.eq(target.view_as(pred))
+          if len(target[incorrects.squeeze(dim=1)])>0:
+            incorrect_pred_targets.append(target[incorrects.squeeze(dim=1)])
+            incorrect_preds.append(pred[incorrects.squeeze(dim=1)])
+            incorrect_pred_images.append(data[incorrects.squeeze(dim=1)])
+  incorrect_pred_targets_all = [t.cpu().numpy() for t in incorrect_pred_targets]
+  incorrect_pred_targets_all = [element for sublist in incorrect_pred_targets_all for element in sublist]
+  incorrect_preds_all = [t.cpu().numpy() for t in incorrect_preds]
+  incorrect_preds_all = [element for sublist in incorrect_preds_all for element in sublist]
+  incorrect_preds_all = [element for sublist in incorrect_preds_all for element in sublist]
+  incorrect_pred_images_all = [element for sublist in incorrect_pred_images for element in sublist]
+
+  print("\n # of incorrect images predicted in test dataset of {}: {}".format(len(test_loader.dataset), len(incorrect_pred_images_all)))
+  figure = plt.figure(figsize = fig_size)
+  num_of_images = num_images
+  for index in range(1, num_of_images + 1):
+      plt.subplot(5, 2, index)
+      # plt.axis('off')
+      # print(incorrect_pred_images_all[index].cpu().numpy().shape)
+      plt.imshow(unnormalize(incorrect_pred_images_all[index].permute(1,2,0).cpu()))
+      plt.title(f'Target- {target_classes[incorrect_pred_targets_all[index]]} ; Predicted- {target_classes[incorrect_preds_all[index]]}')
+  plt.show()
+  print("\n\n crosstab of incorrect prediction to understand patterns: \n")
+  return pd.crosstab(pd.Series([target_classes[i] for i in incorrect_pred_targets_all], name='Actual'), pd.Series([target_classes[i] for i in incorrect_preds_all], name='Predicted'))
