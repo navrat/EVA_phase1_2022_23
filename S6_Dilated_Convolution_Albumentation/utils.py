74346f4dd20f3ea2a72eca44cdb1003e419e3f53
navrat <31410799+navrat@users.noreply.github.com>
2023-02-12 16:07:28 +0530

    Update utils.py

--- S6_Dilated_Convolution_Albumentation/utils.py
+++ S6_Dilated_Convolution_Albumentation/utils.py
@@ -16,20 +16,20 @@ def unnormalize(tensor, mean=(0.4914, 0.4822, 0.4471), std=(0.2469, 0.2433, 0.26
         t.mul_(s).add_(m)
     return tensor
   
- def imshow(img):
+def imshow(img):
     img = unnormalize(img)    # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
-def plot_sample_images(dataloader_obj, batch_size=5)
-# get some random training images
-dataiter = iter(dataloader_obj)
-images, labels = next(dataiter)
-# show images
-imshow(torchvision.utils.make_grid(images[0:batch_size]))
-# print labels
-print([classes[i] for i in labels[0:4]])
+def plot_sample_images(dataloader_obj, target_classes, batch_size=5):
+    # get some random training images
+    dataiter = iter(dataloader_obj)
+    images, labels = next(dataiter)
+    # show images
+    imshow(torchvision.utils.make_grid(images[0:batch_size]))
+    # print labels
+    print([target_classes[i] for i in labels[0:batch_size]])
 
 def plot_mispredictions(model, device, test_loader, target_classes):
   '''
