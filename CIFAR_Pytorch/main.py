8caee088cd502a868dcdbfe3c8d7b50a1fe96c09
navrat <31410799+navrat@users.noreply.github.com>
2023-02-24 15:58:38 +0530

    Update main.py
    updated with onecycle_LR training and testing methods

--- CIFAR_Pytorch/main.py
+++ CIFAR_Pytorch/main.py
@@ -51,15 +51,19 @@ import os
 
 import cv2
 
-from albumentations import Compose, PadIfNeeded, RandomCrop, Normalize, HorizontalFlip, ShiftScaleRotate, CoarseDropout
+from albumentations import Cutout, Compose, PadIfNeeded, RandomCrop, Normalize, HorizontalFlip, ShiftScaleRotate, CoarseDropout
 from albumentations.pytorch.transforms import ToTensorV2
 
 class Compose_Train():
     def __init__(self):
         self.albumentations_transform = Compose([
-            HorizontalFlip(),
-            ShiftScaleRotate(),
-            CoarseDropout (max_holes = 2, max_height=16, max_width=16, min_holes = 1, min_height=4, min_width=4, fill_value=[0.4914*255, 0.4822*255, 0.4471*255], mask_fill_value = None),
+            PadIfNeeded(40),# assignment 8
+            RandomCrop(32,32),# assignment 8
+            HorizontalFlip(),# assignment 8
+            Cutout(num_holes=1, max_h_size=8, max_w_size=8, fill_value=[0.4914*255, 0.4822*255, 0.4471*255], always_apply=True, p=0.50),# assignment 8            
+            # HorizontalFlip(),# assignment 7
+            # ShiftScaleRotate(),# assignment 7
+            # CoarseDropout (max_holes = 2, max_height=16, max_width=16, min_holes = 1, min_height=4, min_width=4, fill_value=[0.4914*255, 0.4822*255, 0.4471*255], mask_fill_value = None),# assignment 7
             Normalize(mean=[0.4914, 0.4822, 0.4471],std=[0.2469, 0.2433, 0.2615]),
             ToTensorV2()
         ])
@@ -265,6 +269,178 @@ def model_training_setup(net, lr = 0.1, criterion="nll_loss", optimizer="sgd", s
   scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
   return criterion, optimizer, scheduler, n_epochs
   
+# Assignment 8 - One cycle learning rate
+def train_onecycle_LR(model, device, train_loader, criterion, scheduler, optimizer, use_l1=False, lambda_l1=0.01):
+    """Function to train the model
+    Args:
+        model (instance): torch model instance of defined model
+        device (str): "cpu" or "cuda" device to be used
+        train_loader (instance): Torch Dataloader instance for trainingset
+        criterion (instance): criterion to used for calculating the loss
+        scheduler (function): scheduler to be used
+        optimizer (function): optimizer to be used
+        use_l1 (bool, optional): L1 Regularization method set True to use . Defaults to False.
+        lambda_l1 (float, optional): Regularization parameter of L1. Defaults to 0.01.
+    Returns:
+        float: accuracy and loss values
+    """
+    model.train()
+    pbar = tqdm(train_loader)
+    lr_trend = []
+    correct = 0
+    processed = 0
+    train_loss = 0
+
+    for batch_idx, (data, target) in enumerate(pbar):
+        # get samples
+        data, target = data.to(device), target.to(device)
+
+        # Init
+        optimizer.zero_grad()
+        # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch 
+        # accumulates the gradients on subsequent backward passes. Because of this, when you start your training loop, 
+        # ideally you should zero out the gradients so that you do the parameter update correctly.
+
+        # Predict
+        y_pred = model(data)
+        # Calculate loss
+        loss = criterion(y_pred, target)
+
+        l1=0
+        if use_l1:
+            for p in model.parameters():
+                l1 = l1 + p.abs().sum()
+        loss = loss + lambda_l1*l1
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        # updating LR
+        if scheduler:
+            if not isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
+                scheduler.step()
+                lr_trend.append(scheduler.get_last_lr()[0])
+
+        train_loss += loss.item()
+
+        # Update pbar-tqdm
+        pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
+        processed += len(data)
+        
+
+        pbar.set_description(desc= f'Batch_id={batch_idx} Loss={train_loss/(batch_idx + 1):.5f} Accuracy={100*correct/processed:0.2f}%')
+    return 100*correct/processed, train_loss/(batch_idx + 1), lr_trend
+
+
+def test_onecycle_LR(model, device, test_loader, criterion):
+    """put model in eval mode and test it
+    Args:
+        model (instance): torch model instance of defined model
+        device (str): "cpu" or "cuda" device to be used
+        test_loader (instance): Torch Dataloader instance for testset
+        criterion (instance): criterion to used for calculating the loss
+    Returns:
+        float: accuracy and loss values
+    """
+    model.eval()
+    test_loss = 0
+    correct = 0
+    #iteration = len(test_loader.dataset)// test_loader.batch_size
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += criterion(output, target).item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    return 100. * correct / len(test_loader.dataset), test_loss
+
+
+def save_model(model, epoch, optimizer, path):
+    """Save torch model in .pt format
+    Args:
+        model (instace): torch instance of model to be saved
+        epoch (int): epoch num
+        optimizer (instance): torch optimizer
+        path (str): model saving path
+    """
+    state = {
+        'epoch': epoch,
+        'state_dict': model.state_dict(),
+        'optimizer': optimizer.state_dict()
+    }
+    torch.save(state, path)
+
+
+def fit_model_onecycle_LR(net, optimizer, criterion, device, NUM_EPOCHS,train_loader, test_loader, use_l1=False, scheduler=None, save_best=False):
+    """Fit the model
+    Args:
+        net (instance): torch model instance of defined model
+        optimizer (function): optimizer to be used
+        criterion (instance): criterion to used for calculating the loss
+        device (str): "cpu" or "cuda" device to be used
+        NUM_EPOCHS (int): number of epochs for model to be trained
+        train_loader (instance): Torch Dataloader instance for trainingset
+        test_loader (instance): Torch Dataloader instance for testset
+        use_l1 (bool, optional): L1 Regularization method set True to use. Defaults to False.
+        scheduler (function, optional): scheduler to be used. Defaults to None.
+        save_best (bool, optional): If save best model to model.pt file, paramater validation loss will be monitered
+    Returns:
+        (model, list): trained model and training logs
+    """
+    training_acc, training_loss, testing_acc, testing_loss = list(), list(), list(), list()
+    lr_trend = []
+    if save_best:
+        min_val_loss = np.inf
+        save_path = 'model.pt'
+
+    for epoch in range(1,NUM_EPOCHS+1):
+        print("EPOCH: {} (LR: {})".format(epoch, optimizer.param_groups[0]['lr']))
+        
+        train_acc, train_loss, lr_hist = train_onecycle_LR(
+            model=net, 
+            device=device, 
+            train_loader=train_loader, 
+            criterion=criterion ,
+            optimizer=optimizer, 
+            use_l1=use_l1, 
+            scheduler=scheduler
+        )
+        test_acc, test_loss = test_onecycle_LR(net, device, test_loader, criterion)
+        # update LR
+        if scheduler:
+            if isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
+                scheduler.step(test_loss)
+        
+        if save_best:
+            if test_loss < min_val_loss:
+                print(f'Valid loss reduced from {min_val_loss:.5f} to {test_loss:.6f}. checkpoint created at...{save_path}\n')
+                save_model(net, epoch, optimizer, save_path)
+                min_val_loss = test_loss
+            else:
+                print(f'Valid loss did not inprove from {min_val_loss:.5f}\n')
+        else:
+            print()
+
+        training_acc.append(train_acc)
+        training_loss.append(train_loss)
+        testing_acc.append(test_acc)
+        testing_loss.append(test_loss)
+        lr_trend.extend(lr_hist)    
+
+    if scheduler:   
+        return net, (training_acc, training_loss, testing_acc, testing_loss, lr_trend)
+    else:
+        return net, (training_acc, training_loss, testing_acc, testing_loss)
+
+
 '''
 ----------
 Earlier training and testing definitions
