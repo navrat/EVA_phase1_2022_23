652575c042522cc90ef7babb06769df809cd1143
navrat <31410799+navrat@users.noreply.github.com>
2023-02-26 23:23:44 +0530

    Update model9.py
    docstrings added

--- CIFAR_Pytorch/models/model9.py
+++ CIFAR_Pytorch/models/model9.py
@@ -37,16 +37,23 @@ Z*Out = 8*8x48 > 48
     return out
 
 class net(nn.Module):
+  '''
+  Add 3 Convolutions to arrive at AxAx48 dimensions (e.g. 32x32x3 | 3x3x3x16 >> 3x3x16x32 >> 3x3x32x48)
+  Apply GAP and get 1x1x48, call this X
+  Repeat the Ultimus block 4 times
+  Then add final FC layer that converts 48 to 10 and sends it to the loss function.
+  Model would look like this C>C>C>U>U>U>U>FFC>Loss
+  '''
   def __init__(self):
     super().__init__()
     self.convblock1 = nn.Sequential(
-        nn.Conv2d(3, 32, 3, 1),
+        nn.Conv2d(3, 16, 3, 1),
+        nn.RELU(),
+        nn.BatchNorm2d(16),
+        nn.Conv2d(16, 32, 3),
         nn.RELU(),
         nn.BatchNorm2d(32),
-        nn.Conv2d(32, 256, 3),
-        nn.RELU(),
-        nn.BatchNorm2d(256),
-        nn.Conv2d(256, 48, 3, 1),
+        nn.Conv2d(32, 48, 3, 1),
         nn.RELU(),
         nn.BatchNorm2d(48),
     )
