7e120c009efcb27e3376a8b837d0247df6b6bb62
navrat <31410799+navrat@users.noreply.github.com>
2023-02-27 00:29:02 +0530

    Update model9.py

--- CIFAR_Pytorch/models/model9.py
+++ CIFAR_Pytorch/models/model9.py
@@ -48,13 +48,13 @@ class Custom_Attention_Model(nn.Module):
   def __init__(self):
     super().__init__()
     self.convblock1 = nn.Sequential(
-        nn.Conv2d(3, 16, 3, 1),
+        nn.Conv2d(3, 16, 3, 1, 1),
         nn.ReLU(),
         nn.BatchNorm2d(16),
-        nn.Conv2d(16, 32, 3),
+        nn.Conv2d(16, 32, 3,1, 1),
         nn.ReLU(),
         nn.BatchNorm2d(32),
-        nn.Conv2d(32, 48, 3, 1),
+        nn.Conv2d(32, 48, 3, 1, 1),
         nn.ReLU(),
         nn.BatchNorm2d(48))
     self.Gap = nn.AdaptiveAvgPool2d((1, 1))
